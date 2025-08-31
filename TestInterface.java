0e32eeb2ec789cd631eb90c8f441e2c790624e48
Mohan123-MKB <mohanbalajibk@gmail.com>
2025-08-31 22:52:36 +0530

    Update TestInterface.java

--- TestInterface.java
+++ TestInterface.java
@@ -1,1 +1,3 @@
-hi
+select name, age, dept, count(*) from employee where salary>50000 group by dept having count(*)>5 order by salary desc limit 5;
+
+group by vantha apply aggregate function use pananum
